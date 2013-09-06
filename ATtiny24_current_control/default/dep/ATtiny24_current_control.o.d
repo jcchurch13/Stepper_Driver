6aecef0dfc74abdc6463a5f1a7f01eeda40c7985
Joe Church <jcchurch@mit.edu>
2013-09-06 02:16:12 -0400

    Original AVR Studio 4 files

---
+++ ATtiny24_current_control/default/dep/ATtiny24_current_control.o.d
@@ -0,0 +1,46 @@
+ATtiny24_current_control.o: ../ATtiny24_current_control.c \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/io.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/inttypes.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/stdint.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/iotn24.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/iotnx4.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/common.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/version.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/lock.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/avr/delay.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/util/delay.h \
+  c:/program\ files/winavr/lib/gcc/../../avr/include/util/delay_basic.h
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/io.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/inttypes.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/stdint.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/iotn24.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/iotnx4.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/common.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/version.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/avr/delay.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/util/delay.h:
+
+c:/program\ files/winavr/lib/gcc/../../avr/include/util/delay_basic.h:
